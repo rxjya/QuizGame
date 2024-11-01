dabce3c4e5eacef510990348d7ac8b4046da6526
RojiyaRai <zmac399@live.rhul.ac.uk>
2024-11-01 15:19:33 +0000

    Update # python quiz game.py
    Added multiple choice options and the correct answers

--- # python quiz game.py
+++ # python quiz game.py
@@ -1,11 +1,30 @@
 # python quiz game
-questions = ("What is an Accumukator(ACC)?",
-             "What is an address bus?",
-             "What is the Arithmetic Logic Unit(ALU)",
-             "What are Buses?",
-             "What is Cache?")
-options = ((),(),(),(),())
-answers = ()
+questions = ("What is an Accumulator(ACC):?",
+             "What is an address bus?:",
+             "What is the Arithmetic Logic Unit(ALU):",
+             "What are Buses?:",
+             "What is Cache?:")
+options = (("A. A special register to temporarily store the results of operations performed by the ALU.", "B. Carries the memory location address of the register the data is being carried
+to or from.", "C. A part of the CPU that performs arithmetic calculations and logical operations on data for the computer programs", "D. A physical set of parallel wires connecting and carrying groups of bits between
+several components of a computer.", "E.A small and fast but expensive memory in the CPU used to store instructions and
+data that are accessed regularly."),
+           ("A. A special register to temporarily store the results of operations performed by the ALU.", "B. Carries the memory location address of the register the data is being carried
+to or from.", "C. A part of the CPU that performs arithmetic calculations and logical operations on data for the computer programs", "D. A physical set of parallel wires connecting and carrying groups of bits between
+several components of a computer.", "E.A small and fast but expensive memory in the CPU used to store instructions and
+data that are accessed regularly."),
+           ("A. A special register to temporarily store the results of operations performed by the ALU.", "B. Carries the memory location address of the register the data is being carried
+to or from.", "C. A part of the CPU that performs arithmetic calculations and logical operations on data for the computer programs", "D. A physical set of parallel wires connecting and carrying groups of bits between
+several components of a computer.", "E.A small and fast but expensive memory in the CPU used to store instructions and
+data that are accessed regularly."),
+           ("A. A special register to temporarily store the results of operations performed by the ALU.", "B. Carries the memory location address of the register the data is being carried
+to or from.", "C. A part of the CPU that performs arithmetic calculations and logical operations on data for the computer programs", "D. A physical set of parallel wires connecting and carrying groups of bits between
+several components of a computer.", "E.A small and fast but expensive memory in the CPU used to store instructions and
+data that are accessed regularly."),
+           ("A. A special register to temporarily store the results of operations performed by the ALU.", "B. Carries the memory location address of the register the data is being carried
+to or from.", "C. A part of the CPU that performs arithmetic calculations and logical operations on data for the computer programs", "D. A physical set of parallel wires connecting and carrying groups of bits between
+several components of a computer.", "E.A small and fast but expensive memory in the CPU used to store instructions and
+data that are accessed regularly."),)
+answers = ("A","B","C","D","E")
 guesses = []
 score = 0
 question_num = 0
