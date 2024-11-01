e9d34b4d0bd7755e0c4ae97e959b57368cd63370
RojiyaRai <zmac399@live.rhul.ac.uk>
2024-11-01 15:07:50 +0000

    Create # python quiz game.py
    Added some tuples of Computer science questions

---
+++ # python quiz game.py
@@ -0,0 +1,11 @@
+# python quiz game
+questions = ("What is an Accumukator(ACC)?",
+             "What is an address bus?",
+             "What is the Arithmetic Logic Unit(ALU)",
+             "What are Buses?",
+             "What is Cache?")
+options = ((),(),(),(),())
+answers = ()
+guesses = []
+score = 0
+question_num = 0
